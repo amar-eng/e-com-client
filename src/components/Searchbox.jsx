81ed7250b7a5c2a4413b1bd2586f50f9df13b6a2
amar-eng <am24mo@gmail.com>
2023-10-16 17:35:49 -0400

    changes to the box

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -2,12 +2,39 @@ import { useEffect, useState } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { Form } from 'react-bootstrap';
 
+function debounce(func, wait) {
+  let timeout;
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
+
 export const SearchBox = ({ reset }) => {
   const navigate = useNavigate();
   const location = useLocation();
-
   const { keyword: urlKeyword } = useParams();
+
   const [keyword, setKeyword] = useState(urlKeyword || '');
+
+  const debouncedNavigate = debounce((keyword) => {
+    if (location.pathname.includes('explore-scents')) {
+      navigate(`/explore-scents/search/${keyword.trim()}`);
+    } else {
+      navigate(`explore-scents/search/${keyword.trim()}`);
+    }
+  }, 300);
+
+  const handleSearch = (e) => {
+    setKeyword(e.target.value);
+    if (e.target.value.length > 0) {
+      debouncedNavigate(e.target.value);
+    }
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -17,11 +44,9 @@ export const SearchBox = ({ reset }) => {
       } else {
         navigate(`explore-scents/search/${keyword.trim()}`);
       }
-      setKeyword('');
     } else {
       navigate('/explore-scents');
     }
-
     setKeyword('');
   };
 
@@ -30,12 +55,13 @@ export const SearchBox = ({ reset }) => {
       setKeyword('');
     }
   }, [reset]);
+
   return (
     <Form onSubmit={submitHandler} className="searchbox">
       <Form.Control
         type="text"
         name="q"
-        onChange={(e) => setKeyword(e.target.value)}
+        onChange={handleSearch}
         value={keyword}
         placeholder="Search Scents..."
         className="searchbox__input"
